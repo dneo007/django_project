df38bdf748d66bfc66a8445915b9d718f978ad84
darren PI <darren.neo@outlook.com>
2020-03-25 21:11:48 +0800

    Allowed feedback on home page

--- blog/models.py
+++ blog/models.py
@@ -5,13 +5,13 @@ from django.urls import reverse
 
 
 class post(models.Model):
-    title = models.CharField(max_length=100, default="title")
+    title = models.CharField(max_length=100, default="  ")
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title  # print out title of objects instead of number of objects returned
+        return self.content  # print out title of objects instead of number of objects returned
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
--- blog/urls.py
+++ blog/urls.py
@@ -3,13 +3,11 @@ from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView,
 from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='blog-home'),
+    path('', views.home, name='blog-home'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('about/', views.about, name='blog-about'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
-    path('addName/', views.addName),
     path('products/', views.showProducts)
 ]
