f72a8a14f68a54f72df310cf1083af01f1447c17
Benjamin Chen <benjaminchen@bechen-a01.vmware.com>
2020-05-27 17:05:29 +0800

    changed banner image in profile

--- my_project/settings.py
+++ my_project/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#   'default': {
-#       'ENGINE': 'django.db.backends.sqlite3',
-#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#   }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangodb',
-        'USER': os.getenv('MYSQL_USER'),
-        'PASSWORD': 'example',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
-    }
+  'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'djangodb',
+#         'USER': os.getenv('MYSQL_USER'),
+#         'PASSWORD': 'example',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
