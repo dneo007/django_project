652dcd9be60b8a2052e897acc9d8cb2c232fa78c
Benjamin Chen <benjaminchen@bechen-a01.vmware.com>
2020-06-05 17:54:43 +0800

    added org bar chart

--- graph/dash_apps/SimpleExample.py
+++ graph/dash_apps/SimpleExample.py
@@ -4,6 +4,9 @@ import dash_html_components as html
 import pandas as pd
 from django_plotly_dash import DjangoDash
 import plotly.express as px
+from users.models import Profile
+from django.db.models import Avg, Count
+
 
 __name__ = 'SimpleExample'
 
@@ -16,68 +19,38 @@ colors = {
     'text': '#7FDBFF'
 }
 
+country_data = {'country': ['Vietnam'],
+                'continent': ['Asia'],
+                'pop': [1],
+                'iso_alpha': ['VNM'],
+                }
+
+country_df = pd.DataFrame(data=country_data)
 df = px.data.gapminder().query("year==2007")
-fig = px.scatter_geo(df, locations="iso_alpha", color="continent",
+fig = px.scatter_geo(country_df, color="continent",
+                        locations="iso_alpha",
                      hover_name="country", size="pop",
                      projection="natural earth")
 
+print(country_df)
+
 print(df)
+
+orgstats = Profile.objects.values('org').annotate(c=Count('org')).order_by('-c').exclude(org='')
+data = {'organization': [], 'count': []}
+
+for orgstat in orgstats:
+    data['organization'].append(orgstat['org'])
+    data['count'].append(orgstat['c'])
+
+data_organization = pd.DataFrame(data=data)
+fig2 = px.bar(data_organization, x='organization', y='count')
+
+
 
 app.layout = html.Div(
     style={'backgroundColor': colors['background']}, children=[
-    dcc.Graph(
-        id='example-graph-2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    ),
-dcc.Graph(
-    figure=dict(
-        data=[
-            dict(
-                x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-                   2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
-                   350, 430, 474, 526, 488, 537, 500, 439],
-                name='Rest of world',
-                marker=dict(
-                    color='rgb(55, 83, 109)'
-                ),
-            ),
-            dict(
-                x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-                   2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
-                   299, 340, 403, 549, 499],
-                name='China',
-                marker=dict(
-                    color='rgb(26, 118, 255)'
-                )
-            )
-        ],
-        layout=dict(
-            title='US Export of Plastic Scrap',
-            showlegend=True,
-            legend=dict(
-                x=0,
-                y=1.0
-            ),
-            margin=dict(l=40, r=0, t=40, b=30)
-        )
-    ),
-    style={'height': 300},
-    id='my-graph'
-),
+    dcc.Graph(figure=fig2),
         dcc.Graph(figure=fig)
 ])
 
